c91505b4c91ea81fb63f7f7af959f878d173e638
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-03-05 17:08:27 +0100

    seeder for services table

--- database/migrations/2024_03_05_133724_create_services_table.php
+++ database/migrations/2024_03_05_133724_create_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
+            $table->text('image');
             $table->timestamps();
         });
     }
