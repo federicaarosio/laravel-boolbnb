e2893cbdfbbe9e50948f277c8a371a61daf47668
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-03-05 15:01:55 +0100

    created model, migration and seeder for services and visitors tables

---
+++ app/Models/Visitor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visitor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'apartment_id',
+        'datetime',
+        'ip_address',
+    ];
+}
