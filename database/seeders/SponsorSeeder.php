e98907e1d823e397b2529dc24d7c5534f4ba2f7f
RobertoScarci <robertoscarci.02@gmail.com>
2024-03-05 16:48:29 +0100

    seeding sponsor

--- database/seeders/SponsorSeeder.php
+++ database/seeders/SponsorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,30 @@ class SponsorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $sponsors = [
+            [
+                'name' => 'Basic',
+                'duration' => '24:00:00',
+                'price' => 2.99
+            ],
+            [
+                'name' => 'Premium',
+                'duration' => '72:00:00',
+                'price' => 5.99
+            ],
+            [
+                'name' => 'Elite',
+                'duration' => '144:00:00',
+                'price' => 9.99
+            ],
+        ];
+
+        foreach($sponsors as $sponsor) {
+            $newSponsor = new Sponsor();
+            $newSponsor->name = $sponsor['name'];
+            $newSponsor->duration = $sponsor['duration'];
+            $newSponsor->price = $sponsor['price'];
+            $newSponsor->save();
+        }
     }
 }
