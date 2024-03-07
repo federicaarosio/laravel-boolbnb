8810924a3cb8ece986c0b08aa9599fb54620e279
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-03-07 09:48:51 +0100

    add index and show api function

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $apartments = Apartment::all();
+        return response()->json([
+            'success' => true,
+            'results' => $apartments
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $apartments = Apartment::with('category', 'services')->findOrFail($id);
+        return response()->json([
+            'success' => true,
+            'results' => $apartments
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
