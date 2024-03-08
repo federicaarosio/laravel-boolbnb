235a5092a542823e6039069d204c058356c8748a
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-03-08 11:31:36 +0100

    add services index api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\ServiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/apartments', ApartmentController::class);
+Route::apiResource('/services', ServiceController::class);
