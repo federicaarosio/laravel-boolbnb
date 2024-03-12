3f634b248470a48ee663859b55556288f5f1fa79
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-03-12 12:22:18 +0100

    add error page

---
+++ resources/views/pages/error.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'error')
+
+@section('main-content')
+    <div id="error" class="container d-flex justify-content-center align-items-center">
+        <h1 class="text-uppercase fw-bold">error 404</h1>
+    </div>
+@endsection
