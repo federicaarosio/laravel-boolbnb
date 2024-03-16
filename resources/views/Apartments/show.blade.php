3f65716954a5c7f5038113a3f4b31c1fadb1f993
Donato-Carola <don94@live.it>
2024-03-16 19:45:28 +0100

    new style show back-end

--- resources/views/Apartments/show.blade.php
+++ resources/views/Apartments/show.blade.php
@@ -5,73 +5,102 @@
 @section('main-content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <div class="col-md-12 pt-3">
-                    <h1 class="font-weight-bold">
-                        {{ $apartment->title }}
-                    </h1>
+            <div class="col-12 mt-2 mb-3">
+                <h1 class="fs-2">
+                    {{ $apartment->title }}
+                </h1>
+            </div>
 
-                    @if ( str_starts_with($apartment->img_url, 'http'))
-                        <img src="{{ $apartment->img_url }}">
-                    @else
-                        <img src="{{ asset('storage') . '/' . $apartment->img_url }}">
-                    @endif
-                    @if ($apartment->sponsors->isNotEmpty())
-                        <div class="my-2">
-                            <span class="badge rounded-pill my-bg-primary me-1 p-2" >Sponsorizzato</span>
-                            Fino al {{ $apartment->sponsors[count($apartment->sponsors) - 1]->pivot->expiry_date }}
-                        </div>
-                    @endif
-
-                    <p class="mt-3">
-                        <strong>Indirizzo:</strong> {{ $apartment->address }}
-                    </p>
-                    <p>
-                        <strong>Descrizione:</strong> {{ $apartment->description }}
-                    </p>
-                    <p>
-                        <strong>Numero di stanze:</strong> {{ $apartment->room_number }}
-                    </p>
-                    <p>
-                        <strong>Numero di letti:</strong> {{ $apartment->bed_number }}
-                    </p>
-                    <p>
-                        <strong>Numero di metri quadrati:</strong> {{ $apartment->square_meters }}
-                    </p>
-
-                    <h3 class="mt-3">Cosa troverai:</h3>
-                    <div class=" d-flex flex-wrap">
-                        @foreach ($apartment->services as $service)
-                            @php
-                                // Crea un nuovo oggetto DOM con la stringa SVG come contenuto
-                                $dom = new DOMDocument();
-                                $dom->loadXML($service->image);
-
-                                // Ottieni il nodo radice SVG
-                                $svgElement = $dom->documentElement;
-
-                                // Serializza il nodo SVG in una stringa
-                                $svg = $dom->saveXML($svgElement);
-                            @endphp
-
-                            <div class="col-md-3 mb-3">
-                                <div class="w-50 h-50 p-1 ">
-                                    {!! $svg !!}
-                                    <span class="d-block text-center mt-2">{{ $service->name }}</span>
-                                </div>
-                            </div>
-                        @endforeach
+                @if (str_starts_with($apartment->img_url, 'http'))
+                    <div class="col-12 d-flex mb-3">
+                        <img class="rounded-4 object-fit-cover w-100 " src="{{ $apartment->img_url }}">
                     </div>
-                    <ul class="list-group mb-5">
-                        @foreach ($apartment->messages as $message)
-                            <li class="list-group-item">
-                                <p class="m-0">messaggio da: {{ $message->name }}</p>
-                                <p>{{ $message->content }}</p>
-                            </li>
-                        @endforeach
-                    </ul>
+                @else
+                    <div class="col-12 d-flex mb-3">
+                        <img class="rounded-4 object-fit-cover w-100 "
+                            src="{{ asset('storage') . '/' . $apartment->img_url }}">
+                    </div>
+                @endif
+
+                <div class="col-7">
+                    <div class="row">
+                        <div class="col-12">
+
+                            <p class="fs-5 fs-md-2 fw-semibold m-0 pt-2">
+                                {{ $apartment->address }}
+                                @if ($apartment->sponsors->isNotEmpty())
+                                    <span class="badge rounded-pill my-bg-primary me-1 p-2">Sponsorizzato</span>
+                                    Fino al
+                                    {{ $apartment->sponsors[count($apartment->sponsors) - 1]->pivot->expiry_date }}
+                                @endif
+                            </p>
+                            <p class="mb-1"> {{ $apartment->bed_number }} bagni · {{ $apartment->room_number }} stanze
+                                · {{ $apartment->square_meters }} metri quadrati </p>
+                            <hr>
+
+                        </div>
+
+                        <div class="col-12">
+                            <div class="d-flex align-items-center ">
+                                <img src="../assets/img/placeholder.svg" class="rounded-circle me-3">
+                                <span>
+                                    <p class="m-0 fw-semibold ">Nome dell'host: {{ $apartment->user->name }}</p>
+
+                                </span>
+                            </div>
+                            <hr>
+                        </div>
+                        <div class="col-12">
+                            <p class="fs-4 fw-semibold ">Descrizione</p>
+                            <p class="pb-3">{{ $apartment->description }}</p>
+                            <hr>
+                        </div>
+                        <div class="col-12">
+                            <p class="fs-4 fw-semibold ">Cosa troverai</p>
+                            <div class=" row">
+
+                                @foreach ($apartment->services as $service)
+                                    @php
+                                        // Crea un nuovo oggetto DOM con la stringa SVG come contenuto
+                                        $dom = new DOMDocument();
+                                        $dom->loadXML($service->image);
+
+                                        // Ottieni il nodo radice SVG
+                                        $svgElement = $dom->documentElement;
+
+                                        // Serializza il nodo SVG in una stringa
+                                        $svg = $dom->saveXML($svgElement);
+                                    @endphp
+                                    <div class="col-5 d-flex mb-3 w-25">
+                                        <div class="w-25 me-2">
+                                            {!! $svg !!}
+                                            <span class="d-block text-center mt-2">{{ $service->name }}</span>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+                <div class="col-5 py-2 ps-5">
+                    <div class="card ms-3 me-1 px-1 ">
+                        <div class="card-body">
+
+                            <ul class="list-group mb-5">
+                                @foreach ($apartment->messages as $message)
+                                    <li class="list-group-item">
+                                        <p class="m-0">messaggio da: {{ $message->name }}</p>
+                                        <p>{{ $message->content }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
