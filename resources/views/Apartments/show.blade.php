b217010586895e8884edadf92f524e8a6fd89fb7
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-03-19 10:10:35 +0100

    add responsive on show

--- resources/views/Apartments/show.blade.php
+++ resources/views/Apartments/show.blade.php
@@ -3,119 +3,19 @@
 @section('title', 'Dettagli Appartamento')
 
 @section('main-content')
-    {{-- <div class="container">
-        <div class="row">
-            <div class="col-12 mt-2 mb-3">
-                <h1 class="fs-2">
-                    {{ $apartment->title }}
-                </h1>
-            </div>
-
-                @if (str_starts_with($apartment->img_url, 'http'))
-                    <div class="col-12 d-flex mb-3">
-                        <img class="rounded-4 object-fit-cover w-100 " src="{{ $apartment->img_url }}">
+    <div id="show" class="container-fluid">
+        <div class="row justify-content-evenly ">
+            <div class="col-12 col-xl-5">
+                <div class="row">
+                    <div class="col-12 mb-2">
+                        <h1 class="fs-2 my-text-primary">
+                            {{ $apartment->title }}
+                        </h1>
                     </div>
-                @else
-                    <div class="col-12 d-flex mb-3">
-                        <img class="rounded-4 object-fit-cover w-100 "
-                            src="{{ asset('storage') . '/' . $apartment->img_url }}">
-                    </div>
-                @endif
-
-                <div class="col-7">
-                    <div class="row">
-                        <div class="col-12">
-
-                            <p class="fs-5 fs-md-2 fw-semibold m-0 pt-2">
-                                {{ $apartment->address }}
-                                @if ($apartment->sponsors->isNotEmpty())
-                                    <span class="badge rounded-pill my-bg-primary me-1 p-2">Sponsorizzato</span>
-                                    Fino al
-                                    {{ $apartment->sponsors[count($apartment->sponsors) - 1]->pivot->expiry_date }}
-                                @endif
-                            </p>
-                            <p class="mb-1"> {{ $apartment->bed_number }} bagni · {{ $apartment->room_number }} stanze
-                                · {{ $apartment->square_meters }} metri quadrati </p>
-                            <hr>
-
+                    <div class="col-12 col-xl-6 mb-2">
+                            <img class="rounded-4 object-fit-cover w-100 h-100" src="{{ $apartment->img_url }}">
                         </div>
-
-                        <div class="col-12">
-                            <div class="d-flex align-items-center ">
-                                <img src="../assets/img/placeholder.svg" class="rounded-circle me-3">
-                                <span>
-                                    <p class="m-0 fw-semibold ">Nome dell'host: {{ $apartment->user->name }}</p>
-
-                                </span>
-                            </div>
-                            <hr>
-                        </div>
-                        <div class="col-12">
-                            <p class="fs-4 fw-semibold ">Descrizione</p>
-                            <p class="pb-3">{{ $apartment->description }}</p>
-                            <hr>
-                        </div>
-                        <div class="col-12">
-                            <p class="fs-4 fw-semibold ">Cosa troverai</p>
-                            <div class=" row">
-
-                                @foreach ($apartment->services as $service)
-                                    @php
-                                        // Crea un nuovo oggetto DOM con la stringa SVG come contenuto
-                                        $dom = new DOMDocument();
-                                        $dom->loadXML($service->image);
-
-                                        // Ottieni il nodo radice SVG
-                                        $svgElement = $dom->documentElement;
-
-                                        // Serializza il nodo SVG in una stringa
-                                        $svg = $dom->saveXML($svgElement);
-                                    @endphp
-                                    <div class="col-5 d-flex mb-3 w-25">
-                                        <div class="w-25 me-2">
-                                            {!! $svg !!}
-                                            <span class="d-block text-center mt-2">{{ $service->name }}</span>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="col-5 py-2 ps-5">
-                    <div class="card ms-3 me-1 px-1 ">
-                        <div class="card-body">
-
-                            <ul class="list-group mb-5">
-                                @foreach ($apartment->messages as $message)
-                                    <li class="list-group-item">
-                                        <p class="m-0">messaggio da: {{ $message->name }}</p>
-                                        <p>{{ $message->content }}</p>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-        
-        <img src="{{ asset('img/wave.svg') }}" class="position-absolute w-100 bottom-0 ">
-        <div id="show" class="container-fluid">
-            <div class="row justify-content-evenly ">
-                <div class="col-5">
-                    <div class="row">
-                        <div class="col-12 mb-2">
-                            <h1 class="fs-2 my-text-primary">
-                                {{ $apartment->title }}
-                            </h1>
-                        </div>
-                        <div class="col-6 mb-2">
-                            <img class="rounded-4 object-fit-cover w-100 " src="{{ $apartment->img_url }}">
-                        </div>
-                        <div class="col-6">
+                        <div class="col-12 col-xl-6">
                             <p class="fw-semibold fs-5 mb-1">{{ $apartment->address }}</p>
                             <p class="fw-semibold my-text-primary mb-2">Caratteristiche</p>
                             <p class="mb-1">{{ $apartment->square_meters }} m<sup>2</sup></p>
@@ -129,19 +29,20 @@
                             </p>
                             <p class="fw-semibold my-text-primary mb-2">
                                 Sponsorizzazione 
-                                @if ($apartment->sponsors->isNotEmpty())
-                                    <span class="badge rounded-pill my-bg-primary me-1 p-2">Attiva</span>
-                                @else
-                                    <span class="badge rounded-pill my-bg-primary me-1 p-2">Non Attiva</span>
-                                @endif
                             </p>
+                            @if ($apartment->sponsors->isNotEmpty())
+                                {{-- <span class="badge rounded-pill my-bg-primary me-1 p-2">Attiva</span> --}}
+                                <p class="mb-1">Sponsor: <span class="my-text-primary fw-semibold ">Attiva</span></p>
+                                @else
+                                {{-- <span class="badge rounded-pill my-bg-primary me-1 p-2">Non Attiva</span> --}}
+                                <p class="mb-1">Sponsor: <span class="my-text-primary fw-semibold ">Non Attiva</span></p>
+                            @endif
                             @if ($apartment->sponsors->isNotEmpty())
                                 <div class="mb-2">
                                     Scade il
                                     {{ \Carbon\Carbon::parse($apartment->sponsors[count($apartment->sponsors) - 1]->pivot->expiry_date)->format('d-m-Y H:i')}}
                                 </div>
                             @endif
-
                         </div>
                         <div class="col-12">
                             <p class="fw-semibold my-text-primary mb-2">Descrizione</p>
@@ -153,10 +54,10 @@
                                         // Crea un nuovo oggetto DOM con la stringa SVG come contenuto
                                         $dom = new DOMDocument();
                                         $dom->loadXML($service->image);
-    
+
                                         // Ottieni il nodo radice SVG
                                         $svgElement = $dom->documentElement;
-    
+
                                         // Serializza il nodo SVG in una stringa
                                         $svg = $dom->saveXML($svgElement);
                                     @endphp
@@ -171,7 +72,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-12 col-xl-4 mt-5 mt-xl-0">
                     <div class="card rounded-4">
                         <div class="card-body">
                             <h1 class="mb-3 my-text-primary fw-medium">Messaggi</h1>
@@ -234,9 +135,7 @@
                 </div>
             </div>
         </div>
-
-
-
-
-
-        @endsection
+    </div>
+    <img src="{{ asset('img/wave.svg') }}" class="w-100 position-absolute bottom-0 d-none d-xl-block">
+    <img src="{{ asset('img/wave.svg') }}" class="w-100 d-block d-xl-none">
+@endsection
