2150785427ddecaae0c306c26605864ae860e4fc
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-03-20 10:59:49 +0100

    fix seeders

--- database/seeders/ApartmentServiceSeeder.php
+++ database/seeders/ApartmentServiceSeeder.php
@@ -19,7 +19,7 @@ class ApartmentServiceSeeder extends Seeder
         $servicesIds = Service::all()->pluck('id');
 
         foreach ($apartments as $apartment) {
-            $apartment->services()->sync($faker->randomElements( $servicesIds, rand(1,5), false ));
+            $apartment->services()->sync($faker->randomElements( $servicesIds, rand(3,8), false ));
         }
 
     }
